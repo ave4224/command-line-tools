ad3141593b6b09ff9ffe595922e978cd3ec2a121
Avery Cowan <avery@bvp.com>
2016-03-09 15:39:13 -0800

    updated

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 directory=$1
 if [ $# -lt 1 ]
 then
@@ -9,7 +10,12 @@ then
   cd $directory
 else
   echo "$directory is not a directory"
-  exit
+  exit 1
+fi
+if git rev-parse --git-dir > /dev/null 2>&1; then
+  :
+  echo "$directory is already in a git repo! Find me a clean place to nest."
+  exit 1
 fi
 relative=`pwd | sed "s/\/Users\/[a-zA-Z0-9_]*/~/"`
 if echo $PATH | egrep -q "$relative|`pwd`"; then
@@ -21,3 +27,4 @@ fi
 git init .
 git remote add -t \* -f origin https://github.com/ave4224/command-line-tools.git
 git checkout master
+exit 0
