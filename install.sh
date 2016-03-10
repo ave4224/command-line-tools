0b4837b4c838df5de20e395a44bc58c8d1eda3bf
Avery Cowan <avery@bvp.com>
2016-03-09 18:23:49 -0800

    updated

--- install.sh
+++ install.sh
@@ -17,20 +17,20 @@ if git rev-parse --git-dir > /dev/null 2>&1; then
   echo "$directory is already in a git repo! Find me a clean place to nest."
   exit 1
 fi
-relative=`echo $directory | sed "s/\/Users\/[a-zA-Z0-9_]*/~/"`
-if echo $PATH | egrep -q "$relative|$directory"; then
+absolute=`pwd`
+relative=`echo $absolute | sed "s/\/Users\/[a-zA-Z0-9_]*/~/"`
+if echo $PATH | egrep -q "$relative|$absolute"; then
   echo "Path already configured"
 else
   echo "export PATH=\"$relative:\$PATH\"" >> ~/.bash_profile
   echo "Path added to ~/.bash_profile"
 fi
-if cat ~/.bash_profile | egrep -q "gitt renew $relative|gitt renew $directory"
+if cat ~/.bash_profile | egrep -q "gitt renew $relative|gitt renew $absolute"
 then
   echo "You've done this before"
 else
   echo "gitt renew $relative" >> ~/.bash_profile
-
-cd $directory
+fi
 git init .
 git remote add -t \* -f origin https://github.com/ave4224/command-line-tools.git
 git checkout master
